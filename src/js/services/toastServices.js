5611b2ed9ce98f1efe064d020d6013eb3c43053f
dribehance <dom4j.for.networking@gmail.com>
2016-06-30 19:12:23 +0800

    add toast less

--- src/js/services/toastServices.js
+++ src/js/services/toastServices.js
@@ -1,7 +1,17 @@
 // by dribehance <dribehance.kksdapp.com>
 angular.module("Server").factory("toastServices", function() {
 	return {
-		show: function() {},
-		hide: function() {}
+		show: function() {
+			$(".toast").show();
+		},
+		hide: function() {
+			$(".toast").hide();
+		},
+		start: function() {
+			NProgress.start();
+		},
+		done: function() {
+			NProgress.done();
+		}
 	}
 });
